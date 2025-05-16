a7116ae4cb46833ab8011cdeb0df94b0e4ba704b
unknown <ungquangtri@gmail.com>
2025-05-16 23:43:20 +0700

    Fetch data for homepage

--- src/components/ItemCard/BlogCard.tsx
+++ src/components/ItemCard/BlogCard.tsx
@@ -1,18 +1,15 @@
-import BlogImage from "../../assets/item2.webp";
-
-export default function BlogCard() {
+export default function BlogCard(props: { blog: any }) {
+  const { blog } = props;
   return (
     <a className="blog-card rounded-xl overflow-hidden w-[167px] md:w-[358px] lg:w-[233px] xl:w-[305px] shadow-xl">
       <img
-        src={BlogImage}
-        className="h-[191px] w-full object-cover object-center hover:scale-115 transition-all duration-500"
+        src={blog.cover_image}
+        alt="Ảnh các blog của người dùng đăng"
+        className="h-[191px] w-full object-cover hover:scale-115 transition-all duration-500"
       />
       <div className="gap-1 flex flex-col px-4 pt-2 pb-4">
-        <span className="genre text-primary uppercase">Romance</span>
-        <span className="line-clamp-3 leading-5">
-          Intense Journeys and Forbidden Desires: A Review of "Chained by
-          Choice: Slavery Series" by LiL A
-        </span>
+        {/* <span className="genre text-primary uppercase">Romance</span> */}
+        <span className="line-clamp-3 leading-5">{blog.content}</span>
       </div>
     </a>
   );
--- src/components/ItemCard/ItemCardV1.tsx
+++ src/components/ItemCard/ItemCardV1.tsx
@@ -1,38 +1,29 @@
-import ItemImage from "../../assets/item1.webp";
-export default function ItemCardV1() {
+export default function ItemCardV1({ story }: { story: any }) {
   return (
     <div className="item-card-v1 w-fit flex gap-3 lg:border-1 lg:border-gray-200 rounded-xl p-3">
       <div className="w-[160px] max-md:w-[116px] flex-none">
-        <img src={ItemImage} className="w-full h-full rounded-lg" />
+        <img
+          src={story.cover_image}
+          className="w-full h-full rounded-lg object-cover"
+        />
       </div>
       <div className="main-info font-spartan gap-2 flex flex-col">
         <div className="title-field font-semibold text-lg leading-5">
-          <span className="">Luna Lola: The Moon Wolf</span>
+          <span className="line-clamp-1">{story.title}</span>
         </div>
         <div className="genre-field text-primary">
-          <span>Werewolf</span>
-        </div>
-        <div className="description-field grow">
-          <span className=" line-clamp-3 text-sm">
-            "You're the moon wolf, Lola. You're the wolf with the power of the
-            Moon goddess", Serena said, and collective gasps were heard in the
-            room. *** After being rejected by her mate in the Moonlit pack, Lola
-            escaped on a full moon only to enter the territory of the next Alpha
-            King, who also happened to be her second-chance mate. Adrian is the
-            next Alpha King, but he hasn't been able to assume his role because
-            he needs Luna by his side. A rogue who trespassed on his territory
-            and whom he ordered to be killed turned out to be his mate, leaving
-            him in a dilemma. Will Adrian reject Lola because she came into his
-            territory as a rogue? Will he overcome what happened to him in the
-            past and give Lola a chance, or reject her and go ahead with Fay as
-            his chosen Luna? What will happen when everyone finds out just how
-            much power Lola wields and how she's supposed to protect her kind in
-            an oncoming war?
+          <span>
+            {story.story_genres.length > 0
+              ? story.story_genres[0].name
+              : "Chưa xác định thể loại"}
           </span>
         </div>
+        <div className="description-field grow">
+          <span className=" line-clamp-3 text-sm">{story.description}</span>
+        </div>
         <div className="flex justify-between text-sm items-center">
-          <span className="whitespace-nowrap">429.2K views</span>
-          <span>Completed</span>
+          <span className="whitespace-nowrap">{story.like_counts} loves</span>
+          <span>{story.progress}</span>
         </div>
       </div>
     </div>
