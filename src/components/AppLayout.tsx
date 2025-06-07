b0dbd6cf1ec083f46a42ccfab6d3ace413c409b0
nngtkhngoc <khanhngoc230705@gmail.com>
2025-06-07 13:14:16 +0700

    fix yarn build

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Chatbot from "@/pages/Chatbot";
-import { Scroll } from "lucide-react";
 import ScrollToTop from "./ScrollToTop";
 function AppLayout() {
   return (
